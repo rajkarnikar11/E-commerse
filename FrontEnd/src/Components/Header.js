f6e321d18cbdde8ce0d10073d7e748f9cc5df6e8
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2021-07-04 13:07:01 +0545

    initial commit

---
+++ FrontEnd/src/Components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Navbar,Nav ,Container,Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+function Header() {
+    return (
+        <header>
+            
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand >My Shop</Navbar.Brand>
+                    </LinkContainer>
+                    
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                
+                    <Navbar.Collapse id="navbarScroll">
+                    
+                        <Nav
+                        className="mr-auto "
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                        >
+                        <LinkContainer to="/cart">
+                            <Nav.Link ><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link ><i className="fas fa-user"></i> Log In</Nav.Link>
+                        </LinkContainer>
+                       
+                        
+                        
+                        </Nav>
+                    
+                        </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
